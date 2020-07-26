4e07bdf2c509498b5af903aa85616ac882c7ad7f
Zach Hall <50154183+ZachJHall@users.noreply.github.com>
2020-07-26 12:10:20 -0500

    #6 | Feature/header (#18)
    * Added header.tsx and installed styled-components types
    
    * Set up header text

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderText = styled.h1`
+
+`
+
+function Header() {
+    return(
+        <HeaderText>Marketing Image Resizer</HeaderText>
+    )
+}
+
+export default Header;
