abbd2691550c5b0224beafc3aa57043450f0bbd0
ZachJHall <zachjohnhall@gmail.com>
2020-07-26 12:21:12 -0500

    Moved header into App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/header';
+
 function App() {
   return (
     <div className="App">
-      <h1>App.tsx</h1>
+      <Header />
     </div>
   );
 }
