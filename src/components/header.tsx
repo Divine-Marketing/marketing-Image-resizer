eca0e4894edef51bc86b53014543ffb88f01e4a3
Carter Wagner <37089795+cjwagn1@users.noreply.github.com>
2020-07-29 14:00:54 -0500

    #21 #27 #20 #5 #9 #23 | Added metadeta, selectors, button, layout, and a few packages (#28)
    * Added metadeta, selectors, button, layout, and a few packages
    
    * Updated read me

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const HeaderText = styled.h1`
-
-`
+  text-align: center;
+`;
 
 function Header() {
-    return(
-        <HeaderText>Marketing Image Resizer</HeaderText>
-    )
+  return <HeaderText>Marketing Image Resizer</HeaderText>;
 }
 
 export default Header;
