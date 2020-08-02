b17c46a20d78151bb0ef890cb5a0c2a88f243a06
Carter Wagner <cjwagn1@gmail.com>
2020-08-02 16:29:07 -0500

    Updates

---
+++ precache-manifest.d6ba445d4b832e2b90acc3477cd0fae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87962aa9a48f37662eea0780eda29c5",
+    "url": "/marketing-Image-resizer/index.html"
+  },
+  {
+    "revision": "e7476013a9b8d9a7ab7e",
+    "url": "/marketing-Image-resizer/static/css/main.aa0024c4.chunk.css"
+  },
+  {
+    "revision": "fcb36d90cd9636d1f13a",
+    "url": "/marketing-Image-resizer/static/js/2.1a205472.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/marketing-Image-resizer/static/js/2.1a205472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7476013a9b8d9a7ab7e",
+    "url": "/marketing-Image-resizer/static/js/main.b22b2928.chunk.js"
+  },
+  {
+    "revision": "3300bdd03c55dca2ace5",
+    "url": "/marketing-Image-resizer/static/js/runtime-main.2b8db07c.js"
+  }
+]);
