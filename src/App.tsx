b8802b4399179e202add852d6ea45ae11a0fad32
Carter Wagner <37089795+cjwagn1@users.noreply.github.com>
2020-10-09 11:45:16 -0500

    Repeat bug fixed (#33)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import "./App.css";
 
@@ -13,57 +12,49 @@ import Upload from "./components/Upload";
 import styled from "styled-components";
 
 const Layout = styled.div`
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
+	padding: 20px;
+	display: flex;
+	flex-direction: column;
 `;
 const Center = styled.div`
-  display: flex;
-  justify-content: center;
+	display: flex;
+	justify-content: center;
 `;
 function App() {
-  const [companyData, setCompanyData] = useState([]);
-  const [imageSizeData, setImageSizeData] = useState([]);
+	const [companyData, setCompanyData] = useState([]);
+	const [imageSizeData, setImageSizeData] = useState([]);
 
-  const [fileData, setFileData] = useState([]);
+	const [fileData, setFileData] = useState([]);
 
+	const companyDataCallback = (data: any) => {
+		setCompanyData(data);
+	};
+	const imageDataCallback = (data: any) => {
+		setImageSizeData(data);
+	};
 
-  const companyDataCallback = (data: any) => {
-    setCompanyData(data);
-  };
-  const imageDataCallback = (data: any) => {
-    setImageSizeData(data);
-  };
+	const fileCallback = (data: any) => {
+		setFileData(data);
+	};
 
-  const fileCallback = (data: any) => {
-    setFileData(data);
-  };
+	return (
+		<div>
+			<Header />
 
-
-  return (
-    <div>
-      <Header />
-
-      <Center>
-        <Layout>
-
-          <Upload fileCallback={fileCallback} />
-          <CompanySelect companyCallback={companyDataCallback} />
-          <SizeSelect imageCallback={imageDataCallback} />
-          <ImageThings
-            file={fileData}
-
-          <CompanySelect companyCallback={companyDataCallback} />
-          <SizeSelect imageCallback={imageDataCallback} />
-          <ImageThings
-
-            companyData={companyData}
-            imageSizeData={imageSizeData}
-          />
-        </Layout>
-      </Center>
-    </div>
-  );
+			<Center>
+				<Layout>
+					<Upload fileCallback={fileCallback} />
+					<CompanySelect companyCallback={companyDataCallback} />
+					<SizeSelect imageCallback={imageDataCallback} />
+					<ImageThings
+						file={fileData}
+						companyData={companyData}
+						imageSizeData={imageSizeData}
+					/>
+				</Layout>
+			</Center>
+		</div>
+	);
 }
 
 export default App;
